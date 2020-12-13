8b67021ca873a17600ea64e27760af75ecf148c3
artemkuzmin66 <70294998+artemkuzmin66@users.noreply.github.com>
2020-12-13 21:51:51 +0300

    main

---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+(() => {
+   const images = document.querySelectorAll(".slider__li")
+   const buttons = document.querySelectorAll(".slider__button")
+   const pages = document.querySelectorAll(".slider__pages-li")
+   const sliderBlock = document.querySelector(".slider")
+
+   const slider = new Slider(
+      images,
+      buttons,
+      pages
+   );
+
+   slider.startSlideShow()
+
+   sliderBlock.addEventListener("click", slider.controllerClick.bind(slider))
+})();
+
+const burger = document.querySelector(".burger")
+const burgerMenu = document.querySelector(".burger__menu")
+
+burger.addEventListener("click", () => {
+   burger.classList.toggle("burger-active")
+   burgerMenu.classList.toggle("burgerMenu-active")
+})
+// burger
+
+const list = document.querySelectorAll(".product__bottom-col-list")
+list.forEach(l => {
+   l.addEventListener('click', (e) => {
+      e.preventDefault()
+      e.target.classList.toggle("product__bottom-col-list-item-active")
+   })
+})
+// list 
+
+const accordion = document.querySelectorAll('.product__bottom-col')
+
+for (item of accordion) {
+   item.addEventListener('click', function () {
+      if (this.classList.contains('active')) {
+         this.classList.remove('active')
+      } else {
+         for (el of accordion) {
+            el.classList.remove('active')
+         }
+         this.classList.add('active')
+      }
+   })
+}
+// accordion
